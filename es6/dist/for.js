698d08f95c866f84ae2bb6edc4665c87806f7650
Alx100 <stotsky.alex@gmail.com>
2017-03-09 20:29:42 +0200

    stage7comlete

---
+++ es6/dist/for.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+// for (let browser in browsers) {
+//     console.log(browser);
+// }
+
+// for (let index in browsers) {
+//     console.log(browsers[index]);
+// }
+
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = browsers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var browser = _step.value;
+
+        console.log(browser);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
+
+console.log(browsers);
