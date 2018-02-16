576710cf8c9f273a78e5b0d829a14debc1121af3
alx100 <stotsky.alex@gmail.com>
2018-02-16 14:55:18 +0200

    Chucks Jokes app completed

---
+++ vanilla-js/chuks-jokes/ajax-external-api.js
@@ -0,0 +1,36 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const input = document.querySelector('input[type="number"]');
+  const number = input.value;
+  // console.log(number);
+  input.value = '';
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('POST', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      console.log(response);
+      
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
