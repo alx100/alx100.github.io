fd78c8891630cefd92a1b568573775c21da54e0c
alx100 <stotsky.alex@gmail.com>
2018-04-11 16:43:33 +0300

    isomorphic articles app completed

---
+++ fullstack/ssr-articles-app/lib/components/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import pickBy from 'lodash.pickby';
+
+import ArticleList from './ArticleList';
+import SearchBar from './SearchBar';
+import Timestamp from './Timestamp';
+
+class App extends React.PureComponent {
+
+  static childContextTypes = {
+    store: PropTypes.object,
+  };
+
+  getChildContext() {
+    return {
+      store: this.props.store
+    };
+  }
+
+  // Partial state update for better performance
+  appState = () => { 
+    const { articles, searchTerm } = this.props.store.getState();
+    return { articles, searchTerm };
+  }
+
+  state = this.appState();
+
+  onStoreChange = () => {
+    this.setState(this.appState);
+  }
+
+  componentDidMount() {
+    this.subscriptionId = this.props.store.subscribe(this.onStoreChange);
+    this.props.store.startClock();
+  }
+
+  componentWillUnmount() {
+    this.props.store.unsubscribe(this.subscriptionId);
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (
+  //     nextState.articles !== this.state.articles
+  //     || nextState.searchTerm !== this.state.searchTerm
+  //   );
+  // }
+
+  render() {
+    let { articles, searchTerm } = this.state;
+    const searchRE = new RegExp(searchTerm, 'i');
+    if(searchTerm) {
+      articles = pickBy(articles, (value) => {
+        return value.title.match(searchRE)
+        || value.body.match(searchRE);
+      });
+    }
+
+    return (
+      <div>
+        <Timestamp />
+        <SearchBar />
+        <ArticleList 
+          articles={articles}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+// Testing for working with async/await after babel-polyfill added
+
+// asyncFunc =() => {  
+//   return Promise.resolve(37);
+// };
+
+// async componentDidMount() {
+//   this.setState({
+//     answer: await this.asyncFunc()
+//   });
+// }
---
+++ fullstack/ssr-articles-app/lib/renderers/dom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from 'components/App';
+import StateApi from 'state-api';
+
+const store = new StateApi(window.initialData);
+
+// const initialData = {
+//   articles: {},
+//   authors: {},
+// };
+
+ReactDOM.render(
+  <App store={store} />,
+  document.getElementById('root')
+);
